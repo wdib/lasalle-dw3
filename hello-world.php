b3c397d7e272abf0ba4f3e6c655dd93dd2716e4f
William Dib <info@williamdib.com>
2025-01-15 10:15:16 -0500

    Lab 1 + print Boolean values

--- hello-world.php
+++ hello-world.php
@@ -1,5 +1,8 @@
 <?php
-  $welcome_message = 'Welcome to DW3!';
+  $welcome_message = 'Welcome to DW3, number of messages in inbox:';
+  $msg_count       = 7;
+  $true_bool       = true;
+  $false_bool      = false;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -11,5 +14,8 @@
 </head>
 <body>
   <h1><?php echo $welcome_message; ?></h1>
+  <p><?php  echo $msg_count ?></p>
+  <p>Boolean true prints: <?php  echo $true_bool ?></p>
+  <p>Boolean false prints: <?php  echo $false_bool ?></p>
 </body>
 </html>
