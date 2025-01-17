a0f7eef705c00e0d8cb19d8f4d8ec3ac4a298836
William Dib <info@williamdib.com>
2025-01-16 21:15:23 -0500

    Created labs/ folder; added labs/1.php

---
+++ labs/1.php
@@ -0,0 +1,16 @@
+<?php
+  $welcome_message = 'Welcome to DW3, number of messages in inbox:';
+  $msg_count       = 7;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lab 1</title>
+</head>
+<body>
+  <h1><?php echo $welcome_message; ?></h1>
+  <p><?php  echo $msg_count; ?></p>
+</body>
+</html>
