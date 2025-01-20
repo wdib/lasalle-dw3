c4938ea6a6611ae73dffacf7f45b20f5e907ad01
William Dib <info@williamdib.com>
2025-01-20 14:25:32 -0500

    Refactored Lab 2

--- labs/2/multidimensional-arrays.php
+++ labs/2/multidimensional-arrays.php
@@ -1,5 +1,5 @@
 <?php 
-$offers = [
+$product_list = [
   [ 'name' => 'Caramel',      'price' => 5, ],
   [ 'name' => 'Cotton candy', 'price' => 3, ],
   [ 'name' => 'Licorice',     'price' => 4, ],
@@ -15,16 +15,16 @@ $offers = [
     <h1>The Candy Store</h1>
     <h2>Offers</h2>
     <p>
-      <?php  echo $offers[ 0 ][ 'name'  ]; ?> -
-      $<?php echo $offers[ 0 ][ 'price' ]; ?>
+      <?php  echo $product_list[ 0 ][ 'name'  ]; ?> -
+      $<?php echo $product_list[ 0 ][ 'price' ]; ?>
     </p>
     <p>
-      <?php  echo $offers[ 1 ][ 'name'  ]; ?> -
-      $<?php echo $offers[ 1 ][ 'price' ]; ?>
+      <?php  echo $product_list[ 1 ][ 'name'  ]; ?> -
+      $<?php echo $product_list[ 1 ][ 'price' ]; ?>
     </p>
     <p>
-      <?php  echo $offers[ 2 ][ 'name'  ]; ?> -
-      $<?php echo $offers[ 2 ][ 'price' ]; ?>
+      <?php  echo $product_list[ 2 ][ 'name'  ]; ?> -
+      $<?php echo $product_list[ 2 ][ 'price' ]; ?>
     </p>
   </body>
 </html>
