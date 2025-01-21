842306e800e78cd66b5921d9ed4eb7279eb1b879
William Dib <info@williamdib.com>
2025-01-21 13:33:39 -0500

    Added string-operators.php

---
+++ string-operators.php
@@ -0,0 +1,23 @@
+<?php
+  $username = 'Sally';
+  $greeting = 'Good evening, ';
+
+  // String concatenation operator (.)
+  $message = $greeting . $username . '!'; // Prints "Good evening, Sally!"
+
+  // String concatenating assignment operator (.=)
+  $greeting .= $username . '!'; // This will make the $greeting variable "Good evening, Sally!"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="main.css" />
+</head>
+<body>
+  <p><?= $message ?></p>
+  <p><?= $greeting ?></p>
+</body>
+</html>
