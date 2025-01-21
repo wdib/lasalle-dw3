36ae8ab1ff4f240d71bf984f3296e28374d1ec91
William Dib <info@williamdib.com>
2025-01-21 13:32:35 -0500

    Added arithmetic-operators.php

---
+++ arithmetic-operators.php
@@ -0,0 +1,25 @@
+<?php
+  $price = 5;
+  $quantity = 12;
+  $total = $price * $quantity;
+
+  // Division operator: /
+  // Multiplication operator: *
+  // Addition operator: +
+  // Subtraction operator: -
+
+  // Increment = Add 1: ++
+  // Decrement = Subtract 1: --
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="main.css" />
+</head>
+<body>
+  <p><?= $total ?></p>
+</body>
+</html>
