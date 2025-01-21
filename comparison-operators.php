f8f48458105c2df9583aa563d3562436654fdb5a
William Dib <info@williamdib.com>
2025-01-21 13:38:40 -0500

    Added comparison-operators.php

---
+++ comparison-operators.php
@@ -0,0 +1,30 @@
+<?php
+  // Is equal to (==)
+  // Is identical to (===)
+
+  // Is not equal to (!=)
+  // Is not identical to (!==)
+
+  // Greater than (>)
+  // Greater than or equal to (>=)
+
+  // Less than (<)
+  // Less than or equal to (<=)
+  $a = 5;
+  $b = '5';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="main.css" />
+</head>
+<body>
+  <p>$a == $b: <?php echo $a == $b; ?></p>
+  <p>$a === $b: <?php echo $a === $b; ?></p>
+  <p>$a != $b: <?php echo $a != $b; ?></p>
+  <p>$a !== $b: <?php echo $a !== $b; ?></p>
+</body>
+</html>
