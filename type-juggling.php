e894f35360da24f78d293444cbdc7424e9af4801
William Dib <info@williamdib.com>
2025-01-21 20:54:03 -0500

    Added type-juggling.php

---
+++ type-juggling.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="main.css" />
+</head>
+<body>
+  <p><?php echo 'Hi ' . 1.23;  ?></p>
+  <p><?php echo 'Hi ' . true;  ?></p>
+  <p><?php echo 'Hi ' . false; ?></p>
+  <p><?php echo 1 + true;      ?></p>
+  <p><?php echo 1 + false;     ?></p>
+  <p><?php echo 1 + '1.2';     ?></p>
+  <p><?php echo 1 + '3.5star'; ?></p>
+  <p><?php //echo 1 + 'star3.5'; ?></p>
+  <p><?php echo true  < 0;     ?></p>
+  <p><?php echo false < 0;     ?></p>
+</body>
+</html>
