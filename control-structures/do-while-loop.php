4f203dffd829066d71c909b7ce06c51a70b558e2
William Dib <info@williamdib.com>
2025-01-27 14:29:34 -0500

    Added loop examples to control-structures/; added table style rules to main.css.

---
+++ control-structures/do-while-loop.php
@@ -0,0 +1,27 @@
+<?php
+  $packs = 5;
+  $price = 1.99;
+?>
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <title>do while Loop</title>
+    <link rel="stylesheet" href="/dw3/main.css">
+  </head>
+  <body>
+    <h1>The Candy Store</h1>
+    <h2>Prices for Multiple Packs</h2>
+    <p>
+    <?php
+      do {
+        echo $packs;
+        echo ' packs cost $';
+        echo $price * $packs;
+        echo '<br>';
+        $packs--;
+      }
+      while ( $packs > 0 );
+    ?>
+    </p>
+  </body>
+</html>
