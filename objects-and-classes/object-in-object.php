7a8b1382b11152a57dfca753f7299291e5f9c44a
William Dib <info@williamdib.com>
2025-02-05 09:15:07 -0500

    Added objects-and-classes/object-in-object.php

---
+++ objects-and-classes/object-in-object.php
@@ -0,0 +1,54 @@
+<?php
+  declare(strict_types = 1);
+
+  class Account {
+    public    AccountNumber $number;
+    public    string        $type;
+    protected float         $balance;
+
+    public function __construct (
+      AccountNumber $number,
+      string        $type,
+      float         $balance = 0.00
+    ) {
+      $this->number  = $number;
+      $this->type    = $type;
+      $this->balance = $balance;
+    }
+
+    public function deposit ( float $amount ): float {
+      $this->balance += $amount;
+      return $this->balance;
+    }
+
+    public function withdraw ( float $amount ): float {
+      $this->balance -= $amount;
+      return $this->balance;
+    }
+
+    public function getBalance (): float {
+      return $this->balance;
+    }
+  }
+
+  class AccountNumber {
+    public int $accountNumber;
+    public int $routingNumber;
+
+    public function __construct ( int $accountNumber, int $routingNumber ) {
+      $this->accountNumber = $accountNumber;
+      $this->routingNumber = $routingNumber;
+    }
+  }
+
+
+  $numbers = new AccountNumber( 12345678, 987654321 );
+
+  $account = new Account( $numbers, 'Savings', 10.00 );
+?>
+
+<?php include 'includes/header.php'; ?>
+
+<h2><?= $account->type ?> Account</h2>
+Account <?= $account->number->accountNumber ?><br>
+Routing <?= $account->number->routingNumber ?><br>
