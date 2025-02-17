1b80b535d5eb811f22616f5e29b62d2b1c56a2aa
William Dib <info@williamdib.com>
2025-02-17 16:25:08 -0500

    Added labs/11/ assets

---
+++ labs/11/includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Dynamic pages — Examples</title>
+    <link href="main.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <div class="content">
+      <div class="page">
+        <header>
+          <a href="index.php">
+            <img src="img/logo.png" alt="Mountain Art Supplies" height="90">
+          </a>
+        </header>
+        <section>
