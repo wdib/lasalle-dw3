e5e4aa5581c98c6b7b9a245209fade25e2f820b7
William Dib <info@williamdib.com>
2025-02-19 09:25:14 -0500

    Updated dynamic-pages/single-field-form.php

--- dynamic-pages/single-field-form.php
+++ dynamic-pages/single-field-form.php
@@ -1,15 +1,34 @@
 <?php include 'includes/header.php'; ?>
 
+<?php
+
+  $data = [
+    'user' => ''
+  ];
+
+  $errors = [];
+
+  if ( $_SERVER[ 'REQUEST_METHOD' ] == 'POST' ) {
+
+    $data[ 'user' ] = isset( $_POST[ 'user' ] ) ? $_POST[ 'user' ] : '';
+    $is_user_valid  = $data[ 'user' ] != '';
+
+    if ( ! $is_user_valid ) {
+      $errors[ 'user' ] = 'Name cannot be empty 😡';
+    }
+  }
+?>
+
 <form action="single-field-form.php" method="POST">
   Name: <input
     type  = "text"
     name  = "user"
-    value = "<?= 
-      isset( $_POST[ 'user' ] )
-      ? htmlspecialchars( $_POST[ 'user' ] )
-      : ''
-    ?>"
+    value = "<?= htmlspecialchars( $data[ 'user' ] ) ?>"
   />
+  <span class="error">
+    <?= isset( $errors[ 'user' ] ) ? $errors[ 'user' ] : '' ?>
+  </span>
+  <br />
   <input type="submit" value="Save" />
 </form>
 
