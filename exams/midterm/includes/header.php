4d227fee16dc0be555f9a903cd788f11c4a121bd
William Dib <info@williamdib.com>
2025-02-24 13:09:55 -0500

    Added midterm exam assets

---
+++ exams/midterm/includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>The Movie Shop</title>
+    <link href="main.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <h1><a href="index.php">The Movie Shop</a></h1>
