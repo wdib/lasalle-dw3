648bc9a72fc8278fedf0483968b572f13c4236ad
William Dib <info@williamdib.com>
2025-03-03 14:01:49 -0500

    Added cookies-sessions/includes/header-style-switcher.php

---
+++ cookies-sessions/includes/header-style-switcher.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Chapter 9 Examples</title>
+    <link href="main.css" rel="stylesheet">
+    <meta charset="UTF-8">
+  </head>
+  <body class="<?= htmlspecialchars($scheme) ?>">
+    <div class="page">
+      <section>
