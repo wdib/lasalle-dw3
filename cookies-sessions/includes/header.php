9a85eec7bc97b228b2cc80cc8dc4ed2e536cac01
William Dib <info@williamdib.com>
2025-03-03 14:01:17 -0500

    Added cookie-sessions/ base assets

---
+++ cookies-sessions/includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Chapter 9 Examples</title>
+    <link href="main.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <meta charset="UTF-8">
+  </head>
+  <body>
+    <div class="page">
+      <section>
